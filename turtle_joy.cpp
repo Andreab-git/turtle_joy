9740d4c1d339b00169961bdf0672d41ffeea7851
Andrea Benà <62255850+Andreab-git@users.noreply.github.com>
2021-01-07 19:50:40 +0100

    Create turtle_joy.cpp

---
+++ turtle_joy.cpp
@@ -0,0 +1,64 @@
+/* Move the turtle using publisher and subscriber in the same node
+
+  author : Andrea Bena'
+  version: 1.0
+  date   : 07/01/2021
+
+*/
+
+#include "ros/ros.h"
+#include "turtlesim/Pose.h"
+#include "sensor_msgs/Joy.h"
+#include "geometry_msgs/Twist.h"
+
+#define INCREMENT 4
+
+
+float move = 0, angle = 0;
+ros::Publisher pub;
+
+void joyCallback(const sensor_msgs::Joy::ConstPtr& msg) {
+ // printf("Move turtle: [%.2f] |--| Turn turtle: [%.2f]\n" , msg->axes[1], msg->axes[2]);
+ // printf("-------------------\n");
+  move = msg->axes[1];
+  angle = msg->axes[2];
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "turtle_joy");
+    ros::NodeHandle node;
+
+    pub = node.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 100);
+    ros::Subscriber sub = node.subscribe("/joy", 100, joyCallback);
+
+    ros::Rate loop(10);
+
+int count = 0;
+
+    while(ros::ok()) {
+
+    geometry_msgs::Twist m;
+    m.linear.x = move*INCREMENT;
+    m.linear.y = 0;
+    m.linear.z = 0;
+
+    m.angular.x = 0;
+    m.angular.y = 0;
+    m.angular.z = angle*INCREMENT;
+
+    pub.publish(m);
+
+
+    ros::spinOnce();
+    loop.sleep();
+    count++;
+
+  }
+    
+    return 0;
+}
+
+
+
+
